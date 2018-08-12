e98b12dbcb9c31f94cbe3db4921aaed380acebc1
Jaanek Liiskmaa <jaanek@eau.org>
2018-08-13 01:52:42 +0300

    Added Thymeleaf templates

---
+++ src/main/java/liiskmaa/spring/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package liiskmaa.spring.spring5webapp.controllers;
+
+import liiskmaa.spring.spring5webapp.model.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model)
+    {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
